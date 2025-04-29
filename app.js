47b5a4690053a9aa97fe3040e92382d2ac3939b2
gohil-gaurav <gohilgaurav89@gmail.com>
2025-04-29 22:31:48 +0530

    Update

--- app.js
+++ app.js
@@ -54,5 +54,99 @@ fetch(json_url).then(Response => Response.json()).then((data) => {
         `
         search.appendChild(card);
     })
+    search_input.addEventListener('keyup', () => {
+        let filter = search_input.value.toUpperCase();
+        let a = search.getElementsByTagName('a');
 
+        for (let i = 0; i < a.length; i++) {
+            let b = a[i].getElementsByClassName('count')[0];
+            // console.log(a.textContent);
+            let TextValue = b.textContent || b.innerText;
+            if(TextValue.toUpperCase().indexOf(filter) > -1){
+                a[i].style.display = "flex";
+                search.style.visibility = "visible";
+                search.style.opacity = "1";
+            }else{
+                a[i].style.display = "none";
+            }
+            if(search_input.value == "0"){
+                search.style.visibility = "hidden";
+                search.style.opacity = "0";
+            }
+        }
+    });
+
+    let video = document.getElementsByTagName('video')[0];
+    let play = document.getElementById('play');
+
+    play.addEventListener('click', () => {
+        if(video.paused){
+            video.play();
+            play.innerHTML = `Play <i class='bi bi-pause-fill'></i>`;
+        }else{
+            video.pause();
+            play.innerHTML = `Watch <i class='bi bi-play-fill'></i>`;
+        }
+    })
+
+    let series = document.getElementById('series');
+    let movies = document.getElementById('movies');
+
+    series.addEventListener('click', () => {
+        cards.innerHTML = '';
+
+        let series_array = data.filter(ele => {
+            return ele.type === "series";
+        });
+
+        series_array.forEach((ele, i) => {
+            let { name, imdb, date, sposter, bposter, genre, url } = ele;
+            let card = document.createElement('a');
+            card.classList.add('card');
+            card.href = url;
+            card.innerHTML = `
+                <img src="${sposter}" alt="${name}" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                        </div>
+                    </div>
+                </div>
+            `
+            cards.appendChild(card);
+        });
+    })
+
+    movies.addEventListener('click', () => {
+        cards.innerHTML = '';
+
+        let movies_array = data.filter(ele => {
+            return ele.type === "movie";
+        });
+
+        movies_array.forEach((ele, i) => {
+            let { name, imdb, date, sposter, bposter, genre, url } = ele;
+            let card = document.createElement('a');
+            card.classList.add('card');
+            card.href = url;
+            card.innerHTML = `
+                <img src="${sposter}" alt="${name}" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                        </div>
+                    </div>
+                </div>
+            `
+            cards.appendChild(card);
+        });
+    })
 });  
